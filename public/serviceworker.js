05aef88454f8ade828fc64024c3b2823f04a8f1e
RolandoGL <vlc960157@gmail.com>
2021-05-27 23:21:01 -0500

    se agregaron los iconos de la aplicacion

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,16 +1,17 @@
 const CACHE_NAME = 'cache_v1_comuniTec',
   urlsToCache = [
-    './offline',
-    './css/app.css',
-    './js/app.js',
-    './images/icons/icon-72x72.png',
-    './images/icons/icon-96x96.png',
-    './images/icons/icon-128x128.png',
-    './images/icons/icon-144x144.png',
-    './images/icons/icon-152x152.png',
-    './images/icons/icon-192x192.png',
-    './images/icons/icon-384x384.png',
-    './images/icons/icon-512x512.png',
+        '/offline',
+        '/css/app.css',
+        '/js/app.js',
+        '/images/icons/icon-72x72.png',
+        '/images/icons/icon-96x96.png',
+        '/images/icons/icon-128x128.png',
+        '/images/icons/icon-144x144.png',
+        '/images/icons/icon-152x152.png',
+        '/images/icons/icon-192x192.png',
+        '/images/icons/icon-384x384.png',
+        '/images/icons/icon-512x512.png',
+
   ]
 
 self.addEventListener('install', e => {
@@ -56,3 +57,5 @@ self.addEventListener('fetch', function(event) {
       })
     );
   });
+
+
